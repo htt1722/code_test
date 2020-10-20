2d8b5139bc2fbca4b32bf2857c81804399080825
hutingting <hutingting@lcnetwork.cn>
2020-10-20 15:18:35 +0800

    commit files

--- src/main/java/exception/MyException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-public class MyException extends RuntimeException{
-    private static final long serialVersionUID = 6334912929831829203L;
-
-    public MyException(String msg) {
-        super(msg);
-    }
-}
